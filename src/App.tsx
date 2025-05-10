af977f1465001562238ab496809d4dfc26263b8a
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-05-10 06:30:09 -0300

    aplicando as mudanças de estado para os meus inputs / appling changes of State to inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import EstiloGlobal, { Container } from './styles'
 import BarraLateral from './containers/BarraLateral'
 import ListaDeContato from './containers/ListaDeContato'
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider, useSelector } from 'react-redux'
+import store, { RootReducer } from './store'
 
+function Conteudo() {
+  const { contatoSelecionado } = useSelector(
+    (state: RootReducer) => state.contato
+  )
+  return (
+    <Container style={{ display: 'flex' }}>
+      <BarraLateral />
+      {contatoSelecionado !== null && <ListaDeContato />}
+    </Container>
+  )
+}
 function App() {
   return (
     <Provider store={store}>
       <EstiloGlobal />
-      <Container>
-        <BarraLateral />
-        <ListaDeContato />
-      </Container>
+      <Conteudo />
     </Provider>
   )
 }
--- src/components/Contato/index.tsx
+++ src/components/Contato/index.tsx
@@ -1,26 +1,109 @@
 import { Tipo } from './style'
 import ContatoState from '../../models/Contato'
+import { useDispatch } from 'react-redux'
+import { editar, limparContatoSelecionado } from '../../store/reducers/contato'
+import { useEffect, useState } from 'react'
 
 type Props = ContatoState
 
-const Contato = ({ id, nome, telefone, email, tipo }: Props) => {
+const Contato = ({
+  id,
+  nome: nomeOriginal,
+  telefone: telefoneOriginal,
+  email: emailOriginal,
+  tipo
+}: Props) => {
+  const [estaEditando, setEstaEditando] = useState(false)
+  const [alteracao, setAlteracao] = useState({
+    nome: nomeOriginal,
+    telefone: telefoneOriginal,
+    email: emailOriginal
+  })
+  useEffect(() => {
+    if (
+      nomeOriginal.length > 0 &&
+      telefoneOriginal.toString.length > 0 &&
+      emailOriginal.length > 0
+    ) {
+      setAlteracao({
+        nome: nomeOriginal,
+        telefone: telefoneOriginal,
+        email: emailOriginal
+      })
+    }
+  }, [nomeOriginal, telefoneOriginal, emailOriginal])
+  function cancelarEdicao() {
+    setEstaEditando(false)
+    setAlteracao({
+      nome: nomeOriginal,
+      telefone: telefoneOriginal,
+      email: emailOriginal
+    })
+  }
+  const dispatch = useDispatch()
+  function ocultacontato() {
+    dispatch(limparContatoSelecionado())
+  }
+  function atualizaCampo(campo: string, valor: string) {
+    setAlteracao((prev) => ({
+      ...prev,
+      [campo]: valor
+    }))
+  }
   return (
-    <div>
-      <label htmlFor={nome}>
-        <input type="text" value={nome} />
-      </label>
-      <label>
-        <input value={telefone} />
-      </label>
-      <label htmlFor={email}>
-        <input type="text" id={email} value={email} />
-      </label>
-      <label htmlFor={tipo}>
-        <Tipo parametro="modo" tipo={tipo}>
-          {tipo}
-        </Tipo>
-      </label>
-    </div>
+    <>
+      {estaEditando ? (
+        <>
+          <button onClick={cancelarEdicao}>Cancelar</button>
+          <button
+            onClick={() => {
+              dispatch(editar({ ...alteracao, id, tipo }))
+              setEstaEditando(false)
+            }}
+          >
+            Salvar
+          </button>
+        </>
+      ) : (
+        <>
+          <button onClick={ocultacontato}>&lt;</button>
+          <button onClick={() => setEstaEditando(true)}>Editar</button>
+        </>
+      )}
+      <div>
+        <label htmlFor={nomeOriginal}>
+          <input
+            type="text"
+            disabled={!estaEditando}
+            onChange={(evento) => atualizaCampo('nome', evento.target.value)}
+            value={alteracao.nome}
+          />
+        </label>
+        <label>
+          <input
+            type="tel"
+            disabled={!estaEditando}
+            onChange={(evento) =>
+              atualizaCampo('telefone', evento.target.value)
+            }
+            value={alteracao.telefone}
+          />
+        </label>
+        <label htmlFor={emailOriginal}>
+          <input
+            type="email"
+            disabled={!estaEditando}
+            value={alteracao.email}
+            onChange={(evento) => atualizaCampo('email', evento.target.value)}
+          />
+        </label>
+        <label htmlFor={tipo}>
+          <Tipo parametro="modo" tipo={tipo}>
+            {tipo}
+          </Tipo>
+        </label>
+      </div>
+    </>
   )
 }
 
